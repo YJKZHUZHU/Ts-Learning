610bfaaaf89c6c14224f27c29565702c03ea51a0
unknown <18470186610@163.com>
2020-05-10 20:06:20 +0800

    ts-init

---
+++ build/webpack.config.js
@@ -0,0 +1,42 @@
+const {
+  resolve
+} = require("path");
+console.log(resolve(__dirname, "../src/index.ts"))
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: resolve(__dirname, "../src/index.ts"),
+  output: {
+    filename: "main.js",
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+      exclude: /node-modules/,
+    }, ],
+  },
+  devtool: (process.env.NODE_ENV = "production" ? false : "inline-source-map"),
+  devServer: {
+    port: 8089,
+    contentBase: resolve(__dirname, "../dist"),
+    stats: 'errors-only',
+    compress: false,
+    host: 'localhost'
+  },
+  resolve: {
+    extensions: [".js", ".ts", "tsx", ".json"],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: [resolve(__dirname, "../dist")],
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, "../src/template/index.html"),
+    }),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const a = '1111'
+
+document.title = 'huihuhui'
+
+console.log(a)
